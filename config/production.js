1750e575dd1899eb8f5ae7fb5e28cdb6b024bd56
kevinccbsg <kevinccbsg@gmail.com>
2021-09-13 00:05:35 +0200

    fix: heroku db errors

--- config/production.js
+++ config/production.js
@@ -2,6 +2,7 @@ module.exports = {
   pg: {
     connection: {
       connectionString: process.env.POSTGRES_CONNECTION_STRING,
+      ssl: { rejectUnauthorized: false },
     },
     schema: 'content',
   },
